546e8a9f5fdfb0c8ffdd168c93484f77b8d89e9a
phatIntern <phat.pham@saleshood.com>
2022-08-17 22:40:46 +0700

    test: render props with fragment

--- src/services/findJSXParentNode.ts
+++ src/services/findJSXParentNode.ts
@@ -1,9 +1,8 @@
+// @ts-nocheck
 import { NodePath } from "@babel/traverse";
 
 export function findJSXParentNode(path: NodePath) {
 	let thatNode;
-	console.log("🚀 ~ file: findJSXParentNode.ts ~ line 6 ~ findJSXParentNode ~ path.parentPath", path.parentPath)
-	console.log("🚀 ~ file: findJSXParentNode.ts ~ line 7 ~ findJSXParentNode ~ path.parentPath.node", path.parentPath.node)
 	do {
 		path = path.parentPath;
 		if (path.parentPath && path.parentPath.node && (path.parentPath.node as any).__node) {
